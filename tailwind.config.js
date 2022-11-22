6892224cca88f92bb544c17bf159413701c2c9a6
Timamyll <104847527+Timamyll@users.noreply.github.com>
2022-11-22 08:22:01 +0600

    Final v2

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./docs/**/*.{html,js,css}"],
+  theme: {
+    container: {
+      padding: "15px",
+      // max-width: '1290px',
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
